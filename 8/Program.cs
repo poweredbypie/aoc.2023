cffb3b695bc3b469bb0f71e3a2141646419626fd
PoweredByPie <emailbypie@gmail.com>
2023-12-08 01:32:36 -0800

    Add part 2 solution for day 8

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -24,7 +24,6 @@ class Solution
         {
             return Node.regex.Match(line).Success;
         }
-
         public Node(string line)
         {
             var match = Node.regex.Match(line);
@@ -35,9 +34,9 @@ class Solution
 
             var groups = match.Groups;
 
-            this.Name = groups[1].Value;
-            this.Left = groups[2].Value;
-            this.Right = groups[3].Value;
+            Name = groups[1].Value;
+            Left = groups[2].Value;
+            Right = groups[3].Value;
         }
     }
 
@@ -47,6 +46,7 @@ class Solution
         int _currMove;
         int _moveCount;
         Dictionary<string, Node> _nodes;
+
         public Map(string[] lines)
         {
             _moves = lines[0]
@@ -72,38 +72,81 @@ class Solution
                 _currMove = 0;
             }
         }
-        Move GetMove()
+        string FollowMove(string name)
         {
-            return _moves[_currMove];
+            var node = _nodes[name];
+            return (_moves[_currMove] == Move.Left) ? node.Left : node.Right;
+        }
+        // Reset the number of moves and the instruction index
+        void Reset()
+        {
+            _currMove = 0;
+            _moveCount = 0;
         }
         // Returns the number of moves required to fully follow the map
-        public int Follow()
+        // Answer to Part A and part of Part B
+        public int FollowLength(string start, Regex end)
         {
-            const string start = "AAA";
-            const string end = "ZZZ";
-
-            for (var current = start; current != end; NextMove())
+            Reset();
+            var current = start;
+            while (!end.IsMatch(current))
             {
-                var node = _nodes[current];
-                var move = GetMove();
-                if (move == Move.Left)
-                {
-                    current = node.Left;
-                }
-                else
-                {
-                    current = node.Right;
-                }
+                current = FollowMove(current);
+                NextMove();
             }
 
             return _moveCount;
         }
-    }
+        // Returns the number of moves required to follow the map
+        // Part B: find coinciding steps for _all_ inputs
+        // Using long because the value is too big otherwise
+        public long FollowCoinciding(Regex start, Regex end)
+        {
+            // All the XXA nodes we need to iterate with
+            var nodes = _nodes
+                .Where(pair => start.IsMatch(pair.Key))
+                .Select(pair => pair.Key)
+                .ToArray();
 
+            // Find the follow length for each of these nodes
+            // This is probably wrong, but:
+            // I thought each node had a "follow cycle" with differing offsets and lengths (like a FSM cycle)
+            // However, running it with all the XXA inputs actually suggests that the values are the same
+            // So I'm making that assumption now
+            var lengths = nodes
+                .Select(node => (long)FollowLength(node, end))
+                .ToArray();
+
+            // The LCM of all of the lengths is the answer
+            return Solution.LeastCommonMultiple(lengths);
+        }
+    }
+    static long GreatestCommonDenominator(long first, long second)
+    {
+        // Euclidean algorithm
+        // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclidean_algorithm
+        while (second != 0)
+        {
+            (first, second) = (second, first % second);
+        }
+        return first;
+    }
+    public static long LeastCommonMultiple(long[] nums)
+    {
+        // Taken from StackOverflow Lol
+        // https://stackoverflow.com/questions/147515/least-common-multiple-for-3-or-more-numbers
+        return nums.Aggregate((first, second) =>
+        {
+            return Math.Abs(first * second) / GreatestCommonDenominator(first, second);
+        });
+    }
     static void Main(string[] args)
     {
         var lines = File.ReadAllLines("input");
         var map = new Map(lines);
-        Console.WriteLine($"Moves needed to follow map is {map.Follow()}");
+        var singular = map.FollowLength("AAA", new Regex("ZZZ"));
+        Console.WriteLine($"Moves needed to follow map from AAA -> ZZZ is {singular}");
+        var multiple = map.FollowCoinciding(new Regex("..A"), new Regex("..Z"));
+        Console.WriteLine($"Moves needed to follow all XXA -> XXZ at the same time is {multiple}");
     }
 }
